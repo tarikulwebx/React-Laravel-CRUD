e01ed048806cafb9d50e1dcb10060e32e114840b
Tarikul <tarikulwebx@gmail.com>
2022-07-01 18:32:18 +0600

    Laravel, React, Vite added

---
+++ resources/js/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <h1>My App</h1>
+  )
+}
+
+export default app
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>React Basic Setup</title>
+
+        @viteReactRefresh
+        @vite(['resources/css/index.css', 'resources/js/index.jsx'])
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+</html>
